8dcb26276e37c1e95339341f778196eb4e6e7393
MATTHEW FISHER <matthewfisher@MATTHEWs-MBP.wp.comcast.net>
2016-07-11 19:50:53 -0400

    add logo image

--- app.js
+++ app.js
@@ -530,10 +530,10 @@ function sendGenericMessage(recipientId) {
         payload: {
           template_type: "generic",
           elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            title: "career",
+            subtitle: "Software Engineering at its finest",
+            item_url: "http://dynamic-memory.com",               
+            image_url: "file://img/logo.png",
             buttons: [{
               type: "web_url",
               url: "https://www.oculus.com/en-us/rift/",
