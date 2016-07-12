f5bfd2147bc34cff297486d09a401ae16d2da861
MATTHEW FISHER <matthewfisher@MATTHEWs-MBP.wp.comcast.net>
2016-07-11 23:32:34 -0400

    add logo imageush

--- app.js
+++ app.js
@@ -533,7 +533,7 @@ function sendGenericMessage(recipientId) {
             title: "Career",
             subtitle: "Software Engineering is awesome",
             item_url: "https://www.dynamic-memory.com/",               
-            image_url: "https://raw.githubusercontent.com/matthewericfisher/fb-robot/master/logo.png",
+            image_url: "https://raw.githubusercontent.com/matthewericfisher/fb-robot/master/danger-man-at-work-hi.png",
             buttons: [{
               type: "web_url",
               url: "https://www.oculus.com/en-us/rift/",
