cc7237ac4993e449e5501b4698c0b55b35423a3f
MATTHEW FISHER <matthewfisher@MATTHEWs-MBP.wp.comcast.net>
2016-07-15 22:05:11 -0400

    another commit

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ function receivedMessage(event) {
   //console.log("Received message for user %d and page %d at %d with message:", 
   //  senderID, recipientID, timeOfMessage);
   //console.log(JSON.stringify(message));
-  console.log("UserInfo: " + userInfo);  
+  console.log("UserInfo: ");  
   //firstName = userInfo.first_name;
 
   var isEcho = message.is_echo;
