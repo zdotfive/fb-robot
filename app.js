f2feec6919dc8d644df785a77d6cf98a55fe5149
MATTHEW FISHER <matthewfisher@MATTHEWs-MBP.wp.comcast.net>
2016-07-17 17:28:03 -0400

    another commit

--- app.js
+++ app.js
@@ -297,7 +297,7 @@ function handleReceivedMessage(event) {
 
     }
   } else if (messageAttachments) {
-    console.log("Received message attachement: " + messageAttachments.payload);
+    console.log("Received message attachement: " + messageAttachments[0].payload);
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
