902ee1ebaec26a85aeeb7e509a5390c42c9afd3a
MATTHEW FISHER <matthewfisher@MATTHEWs-MacBook-Pro.local>
2016-07-17 20:18:29 -0400

    another commit

--- app.js
+++ app.js
@@ -298,7 +298,7 @@ function handleReceivedMessage(event) {
     }
   } else if (messageAttachments) {
     console.log("Received message attachement: " + messageAttachments[0].payload.url);
-    sendJsonMessage(sendID, messageAttachments[0].payload.url);
+    sendJsonMessage(senderID, messageAttachments[0].payload.url);
     //sendTextMessage(senderID, "Message with attachment received");
   }
 }
