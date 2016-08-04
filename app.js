c8064f1564ba9741aef6b5d11324da4e0837b0b0
MATTHEW FISHER <matthewfisher@MATTHEWs-MBP.wp.comcast.net>
2016-08-03 23:41:19 -0400

    another commit

--- app.js
+++ app.js
@@ -1063,8 +1063,8 @@ function callGetLocaleAPI(event, handleReceived) {
     
     if(senderContext[userID])
     {
-       firstName = senderContext[userID]firstName; 
-       lastName = senderContext[userID]lastName; 
+       firstName = senderContext[userID].firstName; 
+       lastName = senderContext[userID].lastName; 
        return;
     }
 
@@ -1083,8 +1083,8 @@ function callGetLocaleAPI(event, handleReceived) {
         firstName = bodyObject.first_name;
         lastName = bodyObject.last_name;
         senderContext[userID] = {};
-        senderContext[userID]firstName = firstName;
-        senderContext[userID]lastName = lastName;
+        senderContext[userID].firstName = firstName;
+        senderContext[userID].lastName = lastName;
         console.log(senderContext);
         handleReceived(event);
       })
@@ -1158,29 +1158,29 @@ var customRules = {};
 function addKeywordStep1(recipientId)
 {
    sendTextMessage(recipientId,"The keyword will drive the actions by the Bot.  The user can type in the keyword or it can be triggered by a link.  The keyword can contain letters, numbers and spaces.  For example, home, i want one and 407 are all valid.  Please type in the keyword:");
-   senderContext[recipientId]state = "addKeywordStep1";
+   senderContext[recipientId].state = "addKeywordStep1";
 }
 
 function addKeywordStep2(recipientId, messageText)
 {
-   senderContext[recipientId]keyword = messageText;
-   senderContext[recipientId]state = "addKeywordStep2";
+   senderContext[recipientId].keyword = messageText;
+   senderContext[recipientId].state = "addKeywordStep2";
    sendJsonMessage(recipientId,"addKeywordStep2");
 }
 
 function stateMachineError(recipientId)
 {
    sendTextMessage(recipientId,"Sorry the Bot is confused.  We will have to start again.");
-   senderContext[recipientId]state = "";
-   senderContext[recipientId]keyword = "";
+   senderContext[recipientId].state = "";
+   senderContext[recipientId].keyword = "";
 }
 
 function addKeywordText(recipientId)
 {
-   if( senderContext[recipientId]state === "addKeywordStep2")
+   if( senderContext[recipientId].state === "addKeywordStep2")
    {
        sendTextMessage(recipientId,"Please type in the text to be sent to the user when this keyword is used.");
-       senderContext[recipientId]state = "addKeywordText";
+       senderContext[recipientId].state = "addKeywordText";
    }
    else
    {
@@ -1190,9 +1190,9 @@ function addKeywordText(recipientId)
 
 function addKeywordTextStep2(recipientId,messageText)
 {
-   if( senderContext[recipientId]state === "addKeywordStep2")
+   if( senderContext[recipientId].state === "addKeywordStep2")
    {
-      var filename = "script"+senderContext[recipientId]keyword;
+      var filename = "script"+senderContext[recipientId].keyword;
       filename = filename.toUpperCase();
       fs.writeFile(filename, '{ \n "recipient\": { \n "id": "recipientId"\n},\n"message": {\n"text": "messageText"\n }\n};', function(err) {
            if(err) {
@@ -1201,8 +1201,8 @@ function addKeywordTextStep2(recipientId,messageText)
            console.log("The file was saved!");
            }
      ); 
-     senderContext[recipientId]state = "";
-     customRules[senderContext.recipientId.keyword.toUpperCase()] = filename;
+     senderContext[recipientId].state = "";
+     customRules[senderContext[recipientId].keyword.toUpperCase()] = filename;
    }
    else
    {
